a3d6cecff8eb26be0604282e8b498c7dc7717710
Guillermo Marinero <gyllestrato@gmail.com>
2024-05-02 19:16:20 -0300

    packages & db string connection added

--- PokemonReviewApp/PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7f0bfbfb0d4c0a8764d460ad79d6b9bde88ad4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143c46f2d5712e6df4492964914df38340af3797")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
