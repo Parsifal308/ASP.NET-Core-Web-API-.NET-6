57018a890e84ebe65a4740ec9efd860f5f9ea38e
Guillermo Marinero <gyllestrato@gmail.com>
2024-05-06 03:01:37 -0300

    modelszzz

---
+++ PokemonReviewApp/PokemonReviewApp/Models/Reviewer.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
