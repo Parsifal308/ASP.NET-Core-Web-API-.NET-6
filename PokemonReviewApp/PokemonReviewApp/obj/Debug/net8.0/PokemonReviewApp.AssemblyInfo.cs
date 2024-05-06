6899af991b4b5017a8c1bc1926c52cd72fb4b637
Guillermo Marinero <gyllestrato@gmail.com>
2024-05-06 05:07:42 -0300

    Database setted, initialized & seeded

--- PokemonReviewApp/PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/PokemonReviewApp/obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d6cecff8eb26be0604282e8b498c7dc7717710")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57018a890e84ebe65a4740ec9efd860f5f9ea38e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
